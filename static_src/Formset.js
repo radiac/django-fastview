8b787fc317f2ce7961b3be4281fb3a17b0521f41
Richard Terry <git@radiac.net>
2020-01-12 23:02:10 +0000

    Add js to add new forms to formsets

--- static_src/Formset.js
+++ static_src/Formset.js
@@ -2,6 +2,18 @@
  * Formset management
  */
 
+const defaultDataFormset = 'fastview-formset';
+const defaultDataForm = 'fastview-formset-form';
+const defaultDataTemplate = 'fastview-formset-template';
+
+const addButton = (formset) => {
+  let button = document.createElement('button');
+  button.innerHTML = 'Add';
+  button.type = 'button';
+  formset.rootEl.appendChild(button);
+  return button;
+};
+
 class Form {
   /**
    * Track forms within a formset
@@ -10,7 +22,7 @@ class Form {
     this.rootEl = rootEl;
     this.prefix = prefix;
 
-    this.deleteEl = `${prefix}-DELETE`;
+    this.deleteEl = rootEl.querySelector(`[name="${prefix}-DELETE"]`);
   }
 
   get isDeleted() {
@@ -39,8 +51,9 @@ export class Formset {
   constructor(
     rootEl,
     prefix,
-    dataForm='fastview-formset-form',
-    dataTemplate='fastview-formset-template',
+    dataForm=defaultDataForm,
+    dataTemplate=defaultDataTemplate,
+    getAddButton=addButton,
   ) {
     this.rootEl = rootEl;
     this.prefix = prefix;
@@ -55,27 +68,61 @@ export class Formset {
     let formEls = rootEl.querySelectorAll(`[data-${dataForm}]`);
     this.forms = []
     formEls.forEach(formEl => {
-      let formPrefix = formEl.dataset[dataForm];
+      let formPrefix = formEl.getAttribute(`data-${dataForm}`);
       let form = new Form(formEl, formPrefix);
       this.forms.push(form);
     });
 
     // Find template form
     this.template = rootEl.querySelector(`[data-${dataTemplate}]`);
+    this.templatePrefix = this.template.getAttribute(`data-${dataTemplate}`);
+
+    // Find and show add button
+    this.addButton = getAddButton(this);
+    this.addButton.onclick = () => {
+      this.add();
+    }
+  }
+
+  add() {
+    // Get new form ID
+    let id = parseInt(this.total_forms.value, 10);
+    id += 1;
+    this.total_forms.value = id;
+
+    // Clone template, replacing __prefix__
+    let template = this.template.innerHTML;
+    let formHtml = template.replace(/__prefix__/g, id);
+    let formRoot = this.template.cloneNode();
+    formRoot.removeAttribute("style");
+    formRoot.innerHTML = formHtml;
+
+    // Add to the end of the existing forms, or after the template if no forms yet
+    let lastForm = this.template;
+    if (this.forms.length > 0) {
+      lastForm = this.forms[this.forms.length - 1].rootEl;
+    }
+    lastForm.parentNode.insertBefore(formRoot, lastForm.nextSibling);
+
+    // Register form
+    let newPrefix = this.templatePrefix.replace('__prefix__', id)
+    let form = new Form(formRoot, newPrefix)
+    this.forms.push(form);
   }
 }
 
 export function formsets(
-  dataFormset='fastview-formset',
-  dataForm='fastview-formset-form',
-  dataTemplate='fastview-formset-template',
+  dataFormset=defaultDataFormset,
+  dataForm=defaultDataForm,
+  dataTemplate=defaultDataTemplate,
+  getAddButton=addButton,
 ) {
   /**
    * Initialise formsets with the data attribute matching dataFormset
    */
   let rootEls = document.querySelectorAll(`[data-${dataFormset}]`);
   rootEls.forEach(rootEl => {
-    let prefix = rootEl.dataset[dataFormset];
-    new Formset(rootEl, prefix, dataForm, dataTemplate);
+    let prefix = rootEl.getAttribute(`data-${dataFormset}`);
+    new Formset(rootEl, prefix, dataForm, dataTemplate, getAddButton);
   });
 }
