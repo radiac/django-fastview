534206c4a2f082773ef3ca6795806e71e94e0519
Richard Terry <git@radiac.net>
2020-02-10 20:37:54 +0000

    Changed js to follow py version

--- tests/test_javascript.py
+++ tests/test_javascript.py
@@ -9,10 +9,9 @@ import fastview
 
 def test_major_minor_versions_match__patch_ignored():
     """
-    Check the major and minor versions in both the Python and JavaScript packages match.
+    Check the versions in both the Python and JavaScript packages match.
 
-    This is a build check to ensure deployments on PyPI and npm stay in sync for
-    ``major.minor`` releases (``major.minor.patch`` does not need to match).
+    This is a build check to ensure deployments on PyPI and npm stay in sync.
     """
     # Get and sanity check the package.json path
     js_package_path = Path(fastview.__file__).parent.parent / "package.json"
@@ -22,8 +21,7 @@ def test_major_minor_versions_match__patch_ignored():
     js_package = json.loads(js_package_path.read_text())
     assert "version" in js_package
 
-    # Drop the patch from the Collect the versions as ``[major, minor, patch`` arrays
-    js_version = js_package["version"].rsplit(".", 1)[0]
-    py_version = fastview.__version__.rsplit(".", 1)[0]
+    js_version = js_package["version"]
+    py_version = fastview.__version__
 
     assert js_version == py_version
